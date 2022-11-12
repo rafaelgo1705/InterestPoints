8b0b85111142c0b68881a53833451ac569221f1e
Rafael Gomes <35879012+rafaelgo1705@users.noreply.github.com>
2022-11-12 19:14:34 -0300

    fix: add sequelize config

---
+++ database/connection.js
@@ -0,0 +1,10 @@
+require("dotenv/config");
+
+module.exports = {
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  dialect: process.env.DATABASE_DIALECT,
+};
