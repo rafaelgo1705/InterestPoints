e7234c5f7a5f8933530fcbb29cdfc1ebe74fc2b1
Rafael Gomes <35879012+rafaelgo1705@users.noreply.github.com>
2022-11-13 00:24:33 -0300

    feat: create table and model to interest point

--- database/migrations/20221112221723-create-table-interest-points.js
+++ database/migrations/20221112221723-create-table-interest-points.js
@@ -4,6 +4,11 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.createTable("interest-points", {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
       name: Sequelize.DataTypes.STRING,
       coordinateX: Sequelize.DataTypes.INTEGER,
       coordinateY: Sequelize.DataTypes.INTEGER,
---
+++ src/2-framework/models/interestPoint/interestPointModel.ts
@@ -0,0 +1,40 @@
+import { Sequelize, DataTypes, Model, Dialect } from "sequelize";
+
+const sequelize = new Sequelize({
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  dialect: process.env.DATABASE_DIALECT as Dialect,
+});
+
+export class InterestPoints extends Model {}
+
+InterestPoints.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    coordinateX: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    coordinateY: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "interest-points",
+    timestamps: true,
+  }
+);
